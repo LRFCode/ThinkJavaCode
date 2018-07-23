884538c819132e0ae1471cc393166d3a4e6649b7
Louis <lrflrf@sbcglobal.net>
2018-07-23 15:34:51 -0500

    formatted

--- ch01/Goodbye.java
+++ ch01/Goodbye.java
@@ -1,12 +1,14 @@
 /**
  * Example program that demonstrates print vs println.
  */
-public class Goodbye {
+public class Goodbye
+{
 
     /**
      * Prints a greeting.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.print("Goodbye, ");  // note the space
         System.out.println("cruel world");
     }
